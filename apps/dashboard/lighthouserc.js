97d60d3e4a50857340c795c7d20fe86b24a01c9b
Aishah Sofea <aishah.s.hanifa@gmail.com>
2025-08-02 22:13:11 +0800

    chore: add more metrics to lighthouse audits as performance budget

--- apps/dashboard/lighthouserc.js
+++ apps/dashboard/lighthouserc.js
@@ -15,9 +15,15 @@ module.exports = {
       preset: "lighthouse:recommended", // or 'lighthouse:no-pwa' or 'lighthouse:all'
       assertions: {
         "categories:performance": ["warn", { minScore: 0.8 }],
-        "categories:accessibility": ["error", { minScore: 0.9 }],
+        "categories:accessibility": ["warn", { minScore: 0.9 }],
         "categories:best-practices": ["warn", { minScore: 0.8 }],
         "categories:seo": ["warn", { minScore: 0.8 }],
+
+        "first-contentful-paint": ["warn", { maxScore: 1.8 }],
+        "speed-index": ["warn", { minScore: 1 }],
+        "total-blocking-time": ["warn", { maxNumericValue: 300 }],
+        "largest-contentful-paint": ["warn", { maxScore: 2 }],
+        "cumulative-layout-shift": ["warn", { maxScore: 0.1 }],
       },
     },
   },
